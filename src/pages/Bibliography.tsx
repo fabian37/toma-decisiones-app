eec40548ef3a24636d452b6ca3fb873e61d2b04d
fabian37 <fabian37gomez@gmail.com>
2023-10-27 10:41:11 -0500

    Agregando loader y bibliografia

--- src/pages/Bibliography.tsx
+++ src/pages/Bibliography.tsx
@@ -1,11 +1,22 @@
+import "./Bibliography.css";
+
 export function Bibliography() {
   return (
     <main className="app-container">
       <section>
         <h1>Bibliografía</h1>
-        <p>
-          Aquí irán las fuentes bibliográficas, recordar citar en normas APA
-        </p>
+        <div className="fuente">
+          <span className="fuente__autor">Mordor Intelligence. (s. f.).</span>
+          <span className="fuente__titulo">
+            {" "}
+            Mercado de lubricantes automotrices de colombia análisis de tamaño y
+            participación tendencias de crecimiento y pronósticos hasta 2026.
+          </span>
+          <span className="fuente__referencia">
+            {" "}
+            https://www.mordorintelligence.com/es/industry-reports/colombia-automotive-lubricants-market
+          </span>
+        </div>
       </section>
     </main>
   );
--- src/pages/Tools.tsx
+++ src/pages/Tools.tsx
@@ -1,9 +1,38 @@
+import "./Tools.css";
+
 export function Tools() {
   return (
     <main className="app-container">
-      <section>
+      <section className="tools">
+        <div
+          aria-label="Orange and tan hamster running in a metal wheel"
+          role="img"
+          className="wheel-and-hamster"
+        >
+          <div className="wheel"></div>
+          <div className="hamster">
+            <div className="hamster__body">
+              <div className="hamster__head">
+                <div className="hamster__ear"></div>
+                <div className="hamster__eye"></div>
+                <div className="hamster__nose"></div>
+              </div>
+              <div className="hamster__limb hamster__limb--fr"></div>
+              <div className="hamster__limb hamster__limb--fl"></div>
+              <div className="hamster__limb hamster__limb--br"></div>
+              <div className="hamster__limb hamster__limb--bl"></div>
+              <div className="hamster__tail"></div>
+            </div>
+          </div>
+          <div className="spoke"></div>
+        </div>
         <h1>Próximamente...</h1>
-        <p>Aquí iran las herramientas de nuestra caja virtual</p>
+        <p>
+          Estamos trabajando arduamente para traerte algo increíble. En breve,
+          estaremos lanzando nuevas productos que te encantarán. Mantente atento
+          para conocer todas las emocionantes actualizaciones que tenemos
+          reservadas para ti.
+        </p>
       </section>
     </main>
   );
