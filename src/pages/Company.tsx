b1b80dbe9d723075c3985b122294fe2a6c5f4ae1
fabian37 <fabian37gomez@gmail.com>
2023-10-27 02:18:58 -0500

    Agregando información de la empresa y realizando el home de la página

--- src/pages/Company.tsx
+++ src/pages/Company.tsx
@@ -1,11 +1,156 @@
+import "./Company.css";
+
 export function Company() {
   return (
     <main className="app-container">
       <section>
-        <h1>Acerca de la empresa a estudiar</h1>
+        <h1>Información de la empresa a analizar</h1>
+        <h2>Historia</h2>
         <p>
-          Colocar aquí la presentación de la empresa a estudiar y toda la
-          información relacionada con ella
+          Distri Lubricantes Kar tiene su origen en el año 2020 en Pandemia por
+          una idea que surgió a raíz de la quiebra de la empresa en la que
+          trabajaba mi mamá Anabel Cardona Guevara la cual se dedicaba a la
+          venta de lubricantes para carros, máquinas etc. Para ese año ella
+          quedó sin trabajo y dijo que no quería trabajarle a otra persona; fue
+          ahí donde con ayuda de mi padre Diego Zamora asesor comercial de la
+          empresa Mundial de Filtros y aceites diseñaron una idea de negocio con
+          base a su largo conocimiento en el sector automotriz en lubricantes y
+          filtros para el mismo.
+        </p>
+        <p>
+          Por otro lado, tuvieron la oportunidad de contar con los clientes
+          antiguos de la otra empresa ya que conocían a mi mamá. Poco a poco
+          fueron buscando un sector adecuado para el puesto de su negocio y para
+          el año 2021 en el mes de febrero iniciaron su actividad económica en
+          un local que está ubicado en el BR obrero con CL 23 # 12 - 52.{" "}
+        </p>
+        <p>
+          Su actividad económica CIIU 4732 (Comercio al por menor de lubricantes
+          (aceites, grasas), aditivos y productos de limpieza para vehículos
+          automotores). Finalmente, ha sido una empresa familiar ya que la
+          conforman Isabel Zamora Cardona (Representante Legal), Anabel Cardona
+          Guevara (Gerente) y Diego Zamora (Asesor comercial).
+        </p>
+        <h2>Descripción</h2>
+        <p>
+          Distri Lubricantes Kar es una empresa distribuidora de Lubricantes al
+          por menor, refrigerantes, aditivos y productos de limpieza para
+          vehículos automotores (CIIU 4732) de marcas como Mobil, Petrobras,
+          Terpel, GP, Chevron, Eni, Lubristone entre otros y filtros de alta
+          calidad, esto con el objetivo de darle al motor una vida útil más
+          prolongada utilizando el aceite correspondiente al tipo de motor y las
+          condiciones de este. Esta empresa se encuentra ubicada en la Ciudad de
+          Cali en la Dirección CLL 23 # 12 - 52 BRR Obrero; contamos con dos
+          trabajadores la Gerente y la asistente administrativa.
+        </p>
+        <h2>Misión</h2>
+        <p>
+          Distri Lubricantes Kar es una empresa distribuidora de lubricantes de
+          marcas como Mobil, Eni, Terpel etc. para el sector automotriz,
+          maquinarias y equipos, satisfaciendo las necesidades del mercado y
+          nuestros clientes, contando para ello con productos indicados de buena
+          calidad, fuerza de ventas idónea llevando así bienestar y desarrollo
+          sostenible a toda la comunidad.
+        </p>
+        <h2>Visión</h2>
+        <p>
+          Para el año 2025, se líder en el mercado de lubricantes a nivel
+          nacional como una empresa orientada a las necesidades del mercado
+          siendo la opción más confiable del mantenimiento del motor;
+          caracterizándonos por nuestro compromiso, excelente trabajo y pasión
+          por lo que hacemos.
+        </p>
+        <h2>Valores</h2>
+        <h3>Valores internos</h3>
+        <p>
+          Son principios éticos y profesionales en los cuales se plasma la
+          identidad de nuestra empresa y deben ser aplicados a todo el talento
+          humano de la organización para poder generar un factor diferenciador a
+          empresas de la competencia y nos ayudará a mejorar procesos y tener un
+          excelente ambiente laboral y profesional:
+        </p>
+        <ul className="company__list">
+          <li>
+            Integridad: Es un valor fundamental para nuestra empresa, se debe
+            aplicar tanto en conjunto como institución e individualmente, para
+            lograr un desarrollo eficiente de buenas prácticas, proyectos y
+            estructuras internas, creando personas éticas y responsables que nos
+            lleven a ser una institución exitosa.
+          </li>
+          <li>
+            Equidad: La equidad es fundamental en nuestra empresa ya que
+            proporcionamos un trato justo y equitativo para todo nuestro talento
+            humano, sin importar su origen, creencias y circunstancias, creando
+            un vínculo emocional entre la empresa y nuestros trabajadores dando
+            como resultado una mayor productividad y lealtad hacia su
+            institución.
+          </li>
+          <li>
+            Honestidad: Es un valor esencial para promover una excelente
+            comunicación, compromiso y responsabilidad basándose en la
+            sinceridad, transparencia y coherencia que debe ser aplicada por
+            nuestro equipo de trabajo y evitando escándalos y problemas legales.
+          </li>
+          <li>
+            Trabajo en equipo: El trabajo en equipo es un valor fundamental para
+            el funcionamiento de nuestra empresa, lograremos el éxito en todos
+            nuestros campos fomentando el trabajo y la colaboración en equipo,
+            apoyando a nuestros colegas en todos los desafíos y problemas a los
+            que nos enfrentemos para así lograr juntos nuestro objetivo en
+            común.
+          </li>
+        </ul>
+        <h3>Valores externos</h3>
+        <p>
+          Los valores externos de una empresa son los que se relacionan con las
+          partes externas de la empresa tales como clientes, proveedores,
+          comunidad y medio ambiente que ayudarán a crear confianza y
+          credibilidad en las partes interesadas:
+        </p>
+        <ul className="company__list">
+          <li>
+            Responsabilidad social: La responsabilidad social de nuestra empresa
+            es el compromiso que tenemos con la sostenibilidad, ética, impacto
+            positivo en la sociedad, responsabilidad y el cumplimiento de las
+            leyes y regulaciones aplicables.
+          </li>
+          <li>
+            Medio ambiente: El medio ambiente es un valor de suma importancia ya
+            que es el compromiso de la empresa con la protección del medio
+            ambiente y la reducción de su impacto ambiental, creando acciones,
+            como la inversión en energías renovables, la reducción de los
+            residuos y promocionar aceites y lubricantes ecológicos.
+          </li>
+        </ul>
+        <h2>
+          ¿Cómo los aceites sintéticos están remplazando los lubricantes
+          tradicionales?
+        </h2>
+        <p>
+          El aceite sintético se usa como un sustituto del lubricante refinado
+          del petróleo cuando se opera a temperaturas extremas porque, en
+          términos generales, proporciona propiedades mecánicas y químicas
+          superiores a las que se encuentran en los aceites minerales
+          tradicionales.
+        </p>
+        <div className="company__title">¿Cómo puede afectar a la empresa?</div>
+        <p>
+          La popularización de aceites sintéticos en el mercado de lubricantes
+          ha tenido un impacto significativo en dicha industria de ventas,
+          algunas de las características por las cuales los aceites sintéticos
+          han afectado esta industria son el rendimiento y durabilidad, los
+          aceites sintéticos generalmente ofrecen un rendimiento superior en
+          comparación con los aceites minerales convencionales, tienen una mejor
+          capacidad para resistir la degradación térmica y proporcionan una
+          lubricación más eficaz a una amplia gama de temperaturas.{" "}
+        </p>
+        <p>
+          Esto ha llevado a una mayor demanda de aceites sintéticos en
+          aplicaciones de alto rendimiento, como vehículos de alto desempeño y
+          maquinaria industrial. Por lo anteriormente mencionado los aceites
+          sintéticos han afectado la venta de lubricantes al cambiar las
+          preferencias del consumidor, impulsar la innovación en la industria y
+          crear desafíos competitivos.
         </p>
       </section>
     </main>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,26 @@
+import { Link } from "react-router-dom";
+import hero from "../../public/hero.jpg";
+import "./Home.css";
+
 export function Home() {
   return (
     <main className="app-container">
-      <section>
-        <h1>
-          Bienvenidos a DeciLab{" "}
-          <span> Laboratorio de Decisiones Empresariales</span>
+      <section className="hero">
+        <h1 className="hero__title">
+          Transforma Desafíos en Oportunidades{" "}
+          <span className="hero__title--detail">Decisiones en Éxito</span>
         </h1>
-        <p>Esta será la página de bienvenida para nuestra aplicación</p>
+        <p>
+          Bienvenido a DeciLab, tu laboratorio digital de soluciones
+          empresariales. En el mundo complejo de los negocios, cada problema es
+          una oportunidad disfrazada y cada decisión es una puerta hacia el
+          éxito.
+        </p>
+        <p>Conoce un poco más sobre nosotros con el siguiente caso de éxito</p>
+        <Link to="/empresa" className="hero__button">
+          Conoce más
+        </Link>
+        <img className="hero__background" src={hero} alt="Toma de decisiones" />
       </section>
     </main>
   );
